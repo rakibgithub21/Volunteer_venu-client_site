f10a21ca18f30a72635815da8a77e4822a04b4e7
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-14 09:38:19 +0600

    edit banner

--- src/pages/Home/Slider.jsx
+++ src/pages/Home/Slider.jsx
@@ -6,6 +6,9 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import image1 from '../../assets/20210918-Beach_Cleanup_Ilhabela-01160.jpg'
 import image2 from '../../assets/25 Ways to Volunteer in Your Community.jpg'
 import image3 from '../../assets/long-term-volunteer-opportunities.jpg'
+import image4 from '../../assets/20210918-Beach_Cleanup_Ilhabela-01160.jpg'
+import image5 from '../../assets/product-5c35e8a4e96e8.[1600].jpeg'
+import image6 from '../../assets/premed volunteering.webp'
 
 const Slider = () => {
     return (
@@ -36,6 +39,24 @@ const Slider = () => {
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="hero min-h-[80vh]" style={{ backgroundImage: `url(${image3})`, backgroundPosition: 'center' }}>
+
+
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: `url(${image4})`, backgroundPosition: 'center' }}>
+
+
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: `url(${image5})`, backgroundPosition: 'center' }}>
+
+
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: `url(${image6})`, backgroundPosition: 'center' }}>
 
 
                     </div>
